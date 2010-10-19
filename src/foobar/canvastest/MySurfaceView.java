c873c67e832cb41b6b468887c3d5430e967bcfec
errorlabs <null@errorlabs>
2010-10-18 21:28:40 -0400

    Created MySurfaceView

---
+++ src/foobar/canvastest/MySurfaceView.java
@@ -0,0 +1,16 @@
+package foobar.canvastest;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created using this guide:
+ * http://developer.android.com/guide/topics/graphics/index.html#on-surfaceview
+ */
+public class MySurfaceView extends SurfaceView {
+
+	public MySurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+}
